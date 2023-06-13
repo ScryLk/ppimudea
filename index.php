eb99652a7b5937d9e8afd3860352a5656b33b3ef
Lucas <lucaskepler991@gmail.com>
2023-06-13 00:39:35 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Mudea</title>
+    <link href="css/styles.css" rel="stylesheet" />
+  </head>
+  <body>
+    <div class="masthead">
+      <div class="masthead-content text-white">
+        <div class="container-fluid px-4 px-lg-0">
+          <h1 class="fst-italic lh-1 mb-4">Mudea</h1>
+          <p class="mb-5">
+            Tudo sobre a sua saúde na palma na palma da sua mão
+          </p>  
+            <div class="row input-group-newsletter">        
+              <div class="col-auto">
+                <button
+                  class="btn btn-primary abled"
+                  id="submitButton"
+                >
+                <a href=""></a>
+                  Me notifique!
+                </button>
+            </div>
+        </div>
+      </div>
+    </div>
+
+  </body>
+</html>
